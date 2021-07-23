19f65727979904e3c48c86196a05b3203757c341
Diplodocus <52214012+DarlingHang@users.noreply.github.com>
2021-07-23 15:13:37 +0800

    remove dependency torchesearchsorted

--- utils/sample_pdf.py
+++ utils/sample_pdf.py
@@ -1,5 +1,4 @@
 import torch
-from torchsearchsorted import searchsorted
 import numpy as np
 '''
 INPUT: 
@@ -45,7 +44,7 @@ def sample_pdf(z_vals, weights, N_samples, det=False, pytest=False):
     # Invert CDF
     u = u.contiguous()
 
-    inds = searchsorted(cdf, u, side='right')
+    inds = torch.searchsorted(cdf, u, side='right')
     below = torch.max(torch.zeros_like(inds-1, device = inds.device), inds-1)
     above = torch.min(cdf.shape[-1]-1 * torch.ones_like(inds, device = inds.device), inds)
     inds_g = torch.stack([below, above], -1)  # (batch, N_samples, 2)
